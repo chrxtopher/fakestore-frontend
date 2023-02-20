5e57bfff5fe23f043710ae2eccfbd407bc49a0e2
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-19 22:40:06 -0700

    Began setting up home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <div className="login">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
