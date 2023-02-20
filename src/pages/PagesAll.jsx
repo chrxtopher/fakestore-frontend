ec44185c9264c7433e31f2bebf905cbc97303367
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-19 23:01:55 -0700

    Began creating sign up page

--- src/pages/PagesAll.jsx
+++ src/pages/PagesAll.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import SignUp from "./SignUp";
 
 function PagesAll() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavButton from "../components/NavButton";
+
+const SignUp = () => {
+  return (
+    <div className="sign-up-page">
+      <p>Sign Up Page</p>
+      <NavButton url="/" title="Home" />
+    </div>
+  );
+};
+
+export default SignUp;
