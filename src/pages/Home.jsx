7c0d4e7d1d6f4ba0b9137fb41093858d33f22e14
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-20 01:55:50 -0700

    Implemented login, logout, and returning active user functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "../components/LoginForm";
+import NavButton from "../components/NavButton";
 
 function Home() {
-  return (
-    <div className="home-page">
-      <div className="login">
-        <LoginForm />
-      </div>
-    </div>
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("loggedIn"));
+  const [activeUser, setActiveUser] = useState(
+    localStorage.getItem("activeUser")
   );
+  console.log(loggedIn);
+
+  const handleLogOut = () => {
+    localStorage.setItem("loggedIn", false);
+    localStorage.removeItem("activeUser");
+    setLoggedIn("false");
+    setActiveUser(null);
+  };
+
+  if (loggedIn === "true" && activeUser && activeUser != "") {
+    return (
+      <div className="continue">
+        <p>{`Continue as ${activeUser}?`}</p>
+        <div className="continue-buttons">
+          <NavButton url={`/dashboard/${activeUser}`} title="Continue" />
+          <button type="button" onClick={handleLogOut}>
+            Log Out
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    localStorage.setItem("loggedIn", false);
+    return (
+      <div className="home-page">
+        <div className="login">
+          <LoginForm />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
