472d66e5cda9a0e742921b63682976badc30e13c
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-20 19:06:24 -0700

    Styled sign up form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import NavButton from "./NavButton";
 import SubmitButton from "./SubmitButton";
 import ErrorCard from "./ErrorCard";
+import "../styles/form.css";
 
 const SignUpForm = () => {
   const [username, setUsername] = useState("");
@@ -27,48 +27,47 @@ const SignUpForm = () => {
   };
 
   return (
-    <form className="sign-up-form">
-      <div className="name-inputs">
-        <label htmlFor="firstName">
-          <p>First Name</p>
-          <input
-            type="text"
-            id="fakeStoreFirstName"
-            value={firstName}
-            onChange={handleFirstNameChange}
-          />
-        </label>
-        <label htmlFor="lastName">
-          <p>Last Name</p>
-          <input
-            type="text"
-            id="fakeStoreLastName"
-            value={lastName}
-            onChange={handleLastNameChange}
-          />
-        </label>
-      </div>
-      <div className="credential-inputs">
-        <label htmlFor="username">
-          <p>Username</p>
-          <input
-            type="text"
-            id="fakeStoreUsername"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </label>
-        <label htmlFor="password">
-          <p>Password</p>
-          <input type="text" value={password} onChange={handlePasswordChange} />
-        </label>
-      </div>
-      <div className="signup-buttons">
-        <SubmitButton title="Create Account" />
-        <p>
-          Already have an account? <a href="/">Login</a>
-        </p>
-      </div>
+    <form className="signup-form">
+      <label htmlFor="firstName" className="custom-field">
+        <input
+          type="text"
+          value={firstName}
+          required
+          onChange={handleFirstNameChange}
+        />
+        <span className="placeholder">First Name</span>
+      </label>
+      <label htmlFor="lastName" className="custom-field">
+        <input
+          type="text"
+          value={lastName}
+          required
+          onChange={handleLastNameChange}
+        />
+        <span className="placeholder">Last Name</span>
+      </label>
+      <label htmlFor="username" className="custom-field">
+        <input
+          type="text"
+          value={username}
+          required
+          onChange={handleUsernameChange}
+        />
+        <span className="placeholder">Username</span>
+      </label>
+      <label htmlFor="password" className="custom-field">
+        <input
+          type="text"
+          value={password}
+          required
+          onChange={handlePasswordChange}
+        />
+        <span className="placeholder">Password</span>
+      </label>
+      <SubmitButton title="Create Account" />
+      <p>
+        Already have an account? <a href="/">Login</a>
+      </p>
       {error.length > 0 && (
         <div className="error-message">
           <ErrorCard message={error} />
