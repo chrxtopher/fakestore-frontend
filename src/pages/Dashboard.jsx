53f68c37dd0cbb459f8ab739453a1745c97539fa
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-20 23:48:48 -0700

    Began setting up Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import LogOutButton from "../components/LogOutButton";
+import axios from "axios";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
+  const url = process.env.REACT_APP_DATABASE_URL || "http://localhost:4050";
   const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async (username) => {
+      const res = await axios.get(`${url}/users/${username}`);
+      setUser(res.data.user);
+    };
+    getUser(username);
+  }, [username, url]);
 
   const logOutClick = () => {
     localStorage.removeItem("activeUser");
     navigate("/");
   };
 
+  const testButton = () => {
+    console.log(user);
+  };
+
   return (
     <div>
+      <button type="button" onClick={testButton}>
+        Test
+      </button>
       <p>Welcome {username}</p>
       <LogOutButton clickHandler={logOutClick} />
     </div>
