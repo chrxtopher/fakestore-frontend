b26668d829997aca62048d87fa66f50ad51302d3
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-21 14:50:32 -0700

    Completed styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import NavItem from "./NavItem";
 import { ReactComponent as ProfileLogo } from "../svgs/profile-icon.svg";
 import "../styles/header.css";
+import ProfileDropDown from "./ProfileDropDown";
 
 const Header = () => {
   return (
     <nav className="nav-bar">
       <ul className="nav-items">
-        <NavItem icon={<ProfileLogo />} />
+        <NavItem icon={<ProfileLogo />} dropDownMenu={<ProfileDropDown />} />
       </ul>
     </nav>
   );
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NavItem = ({ icon, clickHandler }) => {
+const NavItem = ({ icon, dropDownMenu }) => {
+  const [open, setOpen] = useState(false);
+  const clickHandler = () => {
+    setOpen(!open);
+  };
   return (
     <li className="nav-item">
       <button type="button" className="icon-button" onClick={clickHandler}>
         {icon}
       </button>
+      {open && dropDownMenu}
     </li>
   );
 };
