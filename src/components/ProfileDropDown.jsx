5dcb4ced75877559c78e5570ee643ece0429efa3
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-21 17:53:03 -0700

    Added menu buttons to drop down menu

--- src/components/ProfileDropDown.jsx
+++ src/components/ProfileDropDown.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/dropdownMenu.css";
+import { ReactComponent as CartIcon } from "../svgs/cart-icon.svg";
+import { ReactComponent as StarIcon } from "../svgs/star-icon.svg";
+import { ReactComponent as CaretIconRight } from "../svgs/right-caret.svg";
+import MenuButton from "./MenuButton";
+import NavButton from "../components/NavButton";
 import LogOutButton from "../components/LogOutButton";
+import "../styles/dropdownMenu.css";
 
-const ProfileDropDown = () => {
+const ProfileDropDown = ({ cart, wishlist }) => {
   const navigate = useNavigate();
   const logOutClick = () => {
     localStorage.removeItem("activeUser");
@@ -12,6 +17,17 @@ const ProfileDropDown = () => {
 
   return (
     <div className="profile-dropdown">
+      <MenuButton
+        title="Cart"
+        leftIcon={<CartIcon />}
+        rightIcon={<CaretIconRight />}
+      />
+      <MenuButton
+        title="Wish List"
+        leftIcon={<StarIcon />}
+        rightIcon={<CaretIconRight />}
+      />
+      <NavButton title="Edit Profile" />
       <LogOutButton clickHandler={logOutClick} />
     </div>
   );
