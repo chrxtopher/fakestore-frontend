a9fcf40d80d7ee2da4b501411fb77ef327bf0634
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-24 14:56:40 -0700

    Created file that holds functions to fetch API

---
+++ src/util/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const BASE_API_URL =
+  process.env.REACT_APP_BASE_API_URL || "https://fakestoreapi.com";
+
+export async function getOneProduct(productId) {
+  try {
+    const res = await axios.get(`${BASE_API_URL}/products/${productId}`);
+    return res.data;
+  } catch (err) {
+    console.error(`ERROR :: ${err}`);
+  }
+}
+
+export async function getCartProducts(productsIdList) {
+  const productsList = [];
+  for (let i = 0; i < productsIdList.length; i++) {
+    const product = await getOneProduct(productsIdList[i]);
+    productsList.push(product);
+  }
+
+  return productsList;
+}
