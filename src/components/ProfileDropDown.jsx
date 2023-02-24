cbb2eef1ac0687e86dbb41480d2719ef2a5caa71
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-23 22:06:30 -0700

    Moved most of ProfileDropDown to Dashboard at a higher level
    This was to help with props and manipulating them through state

--- src/components/ProfileDropDown.jsx
+++ src/components/ProfileDropDown.jsx
@@ -1,35 +1,8 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { ReactComponent as CartIcon } from "../svgs/cart-icon.svg";
-import { ReactComponent as StarIcon } from "../svgs/star-icon.svg";
-import MenuButton from "./MenuButton";
-import NavButton from "../components/NavButton";
-import LogOutButton from "../components/LogOutButton";
 import "../styles/dropdownMenu.css";
 
-const ProfileDropDown = ({ cart, wishlist }) => {
-  const navigate = useNavigate();
-  const logOutClick = () => {
-    localStorage.removeItem("activeUser");
-    navigate("/");
-  };
-
-  return (
-    <div className="profile-dropdown">
-      <MenuButton
-        title="Cart"
-        leftIcon={`[ ${cart.length} ]`}
-        rightIcon={<CartIcon />}
-      />
-      <MenuButton
-        title="Wish List"
-        leftIcon={`[ ${wishlist.length} ]`}
-        rightIcon={<StarIcon />}
-      />
-      <NavButton title="Edit Profile" />
-      <LogOutButton clickHandler={logOutClick} />
-    </div>
-  );
+const ProfileDropDown = ({ buttons }) => {
+  return <div className="profile-dropdown">{buttons}</div>;
 };
 
 export default ProfileDropDown;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,11 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "../components/Header";
+import { ReactComponent as ProfileLogo } from "../svgs/profile-icon.svg";
+import NavItem from "../components/NavItem";
+import ProfileDropDown from "../components/ProfileDropDown";
+import MenuButton from "../components/MenuButton";
+import NavButton from "../components/NavButton";
+import LogOutButton from "../components/LogOutButton";
+import { ReactComponent as CartIcon } from "../svgs/cart-icon.svg";
+import { ReactComponent as StarIcon } from "../svgs/star-icon.svg";
+import { useNavigate } from "react-router-dom";
+import CartModal from "../components/CartModal";
+import "../styles/dashboard.css";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   const url = process.env.REACT_APP_DATABASE_URL || "http://localhost:4050";
   const { username } = useParams();
   const [user, setUser] = useState(null);
+  const [activeModal, setActiveModal] = useState("none");
 
   useEffect(() => {
     const getUser = async (username) => {
@@ -16,13 +29,61 @@ const Dashboard = () => {
     getUser(username);
   }, [username, url]);
 
-  return (
-    <div className="dashboard">
-      <header>
-        <Header user={user} />
-      </header>
-    </div>
-  );
+  const handleCartClick = () => {
+    setActiveModal("cart");
+  };
+
+  const handleCloseModal = () => {
+    setActiveModal("none");
+  };
+
+  const logOutClick = () => {
+    localStorage.removeItem("activeUser");
+    navigate("/");
+  };
+
+  if (user) {
+    return (
+      <div className="dashboard">
+        <header>
+          <Header
+            user={user}
+            navItems={[
+              <NavItem
+                icon={<ProfileLogo />}
+                dropDownMenu={
+                  <ProfileDropDown
+                    cart={user.cart}
+                    wishlist={user.wishlist}
+                    buttons={[
+                      <MenuButton
+                        title="Cart"
+                        leftIcon={`[ ${user.cart.length} ]`}
+                        rightIcon={<CartIcon />}
+                        clickHandler={handleCartClick}
+                      />,
+                      <MenuButton
+                        title="Wish List"
+                        leftIcon={`[ ${user.wishlist.length} ]`}
+                        rightIcon={<StarIcon />}
+                      />,
+                      <NavButton title="Edit Profile" />,
+                      <LogOutButton clickHandler={logOutClick} />,
+                    ]}
+                  />
+                }
+              />,
+            ]}
+          />
+        </header>
+        {activeModal === "cart" && (
+          <CartModal closeHandler={handleCloseModal} cart={user.cart} />
+        )}
+      </div>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
 };
 
 export default Dashboard;
