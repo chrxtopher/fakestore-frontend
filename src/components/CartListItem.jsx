7c4dede554800bd7f62bf2d465c050b9a8e01c55
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-28 16:54:28 -0700

    Changed what is displayed in each cart item

--- src/components/CartListItem.jsx
+++ src/components/CartListItem.jsx
@@ -7,16 +7,35 @@ const CartListItem = ({ product }) => {
       <div className="cart-product-img">
         <img src={product.image} alt={product.title} />
       </div>
-      <div className="cart-product-info">
+      <div className="product-info">
         <h5>
           {product.title.length > 70
             ? ` [ $${product.price} ] ${product.title.substring(0, 70)}...`
             : `[ $${product.price} ] ${product.title} `}
         </h5>
-        <p>{product.description}</p>
+        <div className="quantity-selection">
+          <form>
+            <label htmlFor="quantity">
+              Quantity
+              <select defaultValue={product.quantity}>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+                <option value={6}>6</option>
+                <option value={7}>7</option>
+                <option value={8}>8</option>
+                <option value={9}>9</option>
+                <option value={10}>10</option>
+              </select>
+            </label>
+          </form>
+        </div>
       </div>
       <div className="cart-product-buttons">
-        <button>X</button>
+        <button className="remove-button">Remove</button>
+        <button className="move-wishlist-button">+ Wishlist</button>
       </div>
     </div>
   );
